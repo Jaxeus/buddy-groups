1ee4f5faa98211be1e097f438a3655dd76c47f49
Jason Cluley <jaxeus@gmail.com>
2024-08-18 16:39:45 +1200

    Changed array from bop to list of colours cycled through with LVL UP button. Added For loop and While loops examples.

--- conditionals-and-switch/script.js
+++ conditionals-and-switch/script.js
@@ -2,30 +2,124 @@
 
 console.log('connected 🔦');
 
-// Changing bgc to lightgray
-document.body.style.backgroundColor = 'lightgray';
+// Changing bgc to light-grey
+document.body.style.backgroundColor = 'rgb(238, 238, 238)';
 
 // Selecting HTML Elements
 const lvlUpBtn = document.querySelector('.lvl-up-btn');
 const counter = document.querySelector('.counter');
+const modeBtn = document.querySelector('.mode-btn');
+const counterDisplay = document.querySelector('.counter-display');
+const loopDisplay = document.querySelector('.loop-display');
 
 // Changing lvl up button and counter styles
 lvlUpBtn.style =
-  'margin: 20px 0 0 20px; color: blue; background-color: hotpink; padding: 5px 10px; border-radius: 7px; font-weight: bold; border: 2px darkgreen solid;';
+  'color: blue; background-color: hotpink; padding: 5px 10px; border-radius: 7px; font-weight: bold; border: 2px darkgreen solid;';
 counter.style =
-  'display: inline-block; width: 70px; text-align: center; margin-left: 20px; padding: 5px 20px; border-radius: 5px; background-color: lightgoldenrodyellow; color: darkgreen; font-weight: bold; border: 2px blue solid;';
+  'display: inline-block; width: 70px; text-align: center;  padding: 5px 20px; border-radius: 5px; background-color: lightgoldenrodyellow; color: darkgreen; font-weight: bold; border: 2px blue solid;';
 
 // Setting count as 0 so that function can change count on each click
 let count = 0;
 
 // Function for lvl-up button to increase the count of counter by one each click
 function increaseCount() {
-  count++;
+  count === 5 ? (count = 0) : count++;
+  // If count === 5 ? (then perform) : otherwise perform this
+  // ! Ternary
   counter.textContent = count;
-  console.log(count);
+
+  console.log('Count is:', count);
+
+  // ! Switch
+  switch (count) {
+    case 0:
+      counterDisplay.style.backgroundColor = 'white';
+      break;
+
+    case 1:
+      counterDisplay.style.backgroundColor = 'lime';
+      break;
+
+    case 2:
+      counterDisplay.style.backgroundColor = 'salmon';
+      break;
+
+    case 3:
+      counterDisplay.style.backgroundColor = 'violet';
+      break;
+
+    case 4:
+      counterDisplay.style.backgroundColor = 'yellow';
+      break;
+
+    case 5:
+      counterDisplay.style.backgroundColor = 'aqua';
+      break;
+
+    default:
+      console.log('Switch not working!');
+  }
 }
 
 // Adding event listener for lvl up button through JS instead of onclick
 lvlUpBtn.addEventListener('click', increaseCount);
 
-// ! Conditional statement
+// Dark-mode: #3b3b3b Light-mode: #eeeeee
+const darkMode = 'rgb(59, 59, 59)';
+const lightMode = 'rgb(238, 238, 238)';
+
+// ! Conditional statements
+
+// * Dark-mode/Light-mode
+
+let isDarkMode = false;
+
+const toggleMode = () => {
+  console.log('Mode Button hit');
+
+  isDarkMode = !isDarkMode;
+  console.log(isDarkMode);
+
+  if (isDarkMode) {
+    document.body.style.backgroundColor = darkMode;
+    modeBtn.style = 'background-color: white; color: black;';
+    modeBtn.textContent = 'I LIKE IT BRIGHT 💡';
+  } else {
+    document.body.style.backgroundColor = lightMode;
+    modeBtn.style =
+      'margin-right: 30px; padding: 5px 20px; border-radius: 7px; font-weight: bold; background-color: black;color: white; border: 2px darkgreen solid;';
+    modeBtn.textContent = 'AAHH MY EYES!! 😵';
+  }
+};
+
+modeBtn.addEventListener('click', toggleMode);
+
+// ! Loop Container and Display
+
+// Creating an array of the colours the switch statement above cycles through when the LVL UP button is clicked.
+const colourArrayToLoop = [
+  'white',
+  'lime',
+  'salmon',
+  'violet',
+  'yellow',
+  'aqua',
+];
+
+// Initialising colourList to an empty string so that when we include colourList in a loop, we can add some paragraph after paragraph (HTML) into this string
+let colourList = '';
+
+// ! For Loop
+// for (let i = 0; i < colourArrayToLoop.length; i++) {
+//   colourList += `<p style='display: inline; background-color: ${colourArrayToLoop[i]}; padding: 5px 10px; border: solid 1px black;'>${colourArrayToLoop[i]}</p><br/>`;
+// }
+
+// ! While Loop
+// Initialising index as 0 so that index in the while loop starts at the first index of the colourArrayToLoop
+let index = 0;
+while (index < colourArrayToLoop.length) {
+  colourList += `<p style='display: inline; background-color: ${colourArrayToLoop[index]}; padding: 5px 10px; border: solid 1px black;'>${colourArrayToLoop[index]}</p><br/>`;
+  index++;
+}
+
+loopDisplay.innerHTML = colourList;
